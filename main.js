e6cac7ed06c3ac6aa8d45f5c8725124eb7abab85
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2024-03-25 10:30:22 -0500

    Add statistics.js and Google Charts

--- main.js
+++ main.js
@@ -1,24 +1,47 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import Statistics from 'statistics.js'
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+  <div>Hello World</div>
+  <div id="chart_div"></div>
+`;
+var data = [
+	{ ID: 1, age: 33 },
+	{ ID: 2, age: 42 },
+	{ ID: 3, age: 27 }
+];
 
-setupCounter(document.querySelector('#counter'))
+var columns = {
+	ID: 'ordinal',
+	age: 'interval'
+};
+
+var settings = { };
+
+var stats = new Statistics(data, columns, settings);
+
+var meanAge = stats.arithmeticMean("age");
+var stdDevAge = stats.standardDeviation("age");
+
+console.log('test', meanAge, stdDevAge);
+
+// Charts Test
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Topping');
+  data.addColumn('number', 'Slices');
+  data.addRows([
+    ['Mushrooms', 3],
+    ['Onions', 1],
+    ['Olives', 1],
+    ['Zucchini', 1],
+    ['Pepperoni', 2]
+  ]);
+  var options = {'title':'How Much Pizza I Ate Last Night',
+                 'width':400,
+                 'height':300};
+  var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+  chart.draw(data, options);
+}
